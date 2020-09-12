caaa6157355cfdc0b7febbc87539d49783266509
KaMeHb-UA <marlock@etlgr.com>
2020-09-12 15:57:31 +0300

    mfcoin as default network

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-new Function('a', 'module.exports = a')(require('bitcoinjs-lib'))
+const bjs = require('bitcoinjs-lib');
+
+const networks = Object.assign({
+    mfcoin: {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bech32: 'bc',
+        bip32: {
+            public: 0x0476b21e,
+            private: 0x0476ade4,
+        },
+        pubKeyHash: 0x33,
+        scriptHash: 0x05,
+        wif: 0xb3,
+    }
+}, bjs.networks);
+
+bjs.networks.bitcoin = networks.mfcoin;
+
+new Function('a', 'module.exports = a')(Object.assign({}, bjs, { networks }))
